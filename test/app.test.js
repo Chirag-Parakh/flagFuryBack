c0a2adf2c8f43d8bf19ba36a169b9c59f5a3e4ea
Chirag Parakh <cparakh53@gmail.com>
2024-05-08 00:40:16 +0530

    final

--- test/app.test.js
+++ test/app.test.js
@@ -33,50 +33,34 @@ const request = require('supertest');
 // const expect = require('chai').expect;
 // const expect = import('chai').then(chai => chai.expect);
 
-describe('GET /', function() {
-  it('responds with status 200 and "this is index.js page"', function(done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect('this is index.js page', done);
-  });
+describe('GET /', function () {
+    it('responds with status 200 and "this is index.js page"', function (done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect('this is index.js page', done);
+    });
 });
+describe('POST /login', function () {
+    it('responds with status 404 when user does not exist', function (done) {
+        request(app)
+            .post('/login')
+            .send({ username: 'nonexistentuser', password: 'invalidpassword' })
+            .expect(404)
+            .expect('Content-Type', /json/)
+            .expect({ message: "User doesn't exist" }, done);
+    });
+    });
+    // Add more test cases for other endpoints...
 
-// Add more test cases for other endpoints...
+    // Start the Express server on port 3001 for testing
+    const server = app.listen(3001, () => {
+        console.log('Testing server running on port 3001');
+    });
 
-// Start the Express server on port 3001 for testing
-const server = app.listen(3001, () => {
-  console.log('Testing server running on port 3001');
-});
+    // Close the server after all tests are done
+    after(function (done) {
+        server.close(done);
+    });
 
-// Close the server after all tests are done
-after(function(done) {
-  server.close(done);
-});
-
-// import('chai').then(chai => {
-//     const expect = chai.expect;
-  
-//     describe('GET /', function() {
-//       it('responds with status 200 and "this is index.js page"', function(done) {
-//         request(app)
-//           .get('/')
-//           .expect(200)
-//           .expect('this is index.js page', done);
-//       });
-//     });
-  
-//     // Add more test cases for other endpoints...
-  
-//     // Start the Express server on port 3001 for testing
-//     const server = app.listen(3001, () => {
-//       console.log('Testing server running on port 3001');
-//     });
-  
-//     // Close the server after all tests are done
-//     after(function(done) {
-//       server.close(done);
-//     });
-//   });
-  
 
